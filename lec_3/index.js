c8ac19dc600404f2c77dec70119b9ca10f54b024
shubh <ssagar8186@gmail.com>
2025-02-07 08:39:41 +0530

    added code

--- lec_3/index.js
+++ lec_3/index.js
@@ -3,4 +3,9 @@ var myInput = document.getElementById('myInput')
 
 myModal.addEventListener('shown.bs.modal', function () {
   myInput.focus()
+  let modal2 = document.getElementById('modalBack');
+  modal2.style.background = 'red';
+  modal4.addEventListener('click', (e) =>{
+    modal2.style.background = 'green';
+  })
 })
